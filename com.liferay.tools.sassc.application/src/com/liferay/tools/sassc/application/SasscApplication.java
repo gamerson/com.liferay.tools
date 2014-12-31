b8e69dbe26f6c5e3fc301708f212d94b45dd298e
Gregory Amerson <gregory.amerson@liferay.com>
2014-12-31 12:00:47 +0800

    Renamed executable jars

--- com.liferay.tools.sassc.application/src/com/liferay/tools/sassc/application/SasscApplication.java
+++ com.liferay.tools.sassc.application/src/com/liferay/tools/sassc/application/SasscApplication.java
@@ -1,5 +1,7 @@
 package com.liferay.tools.sassc.application;
 
+import java.util.Map;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -10,15 +12,20 @@ import osgi.enroute.debug.api.Debug;
 
 @Component(
 		name="com.liferay.tools.sassc",
-		service=SasscApplication.class,
+		service= {
+				SasscApplication.class,
+				Runnable.class
+		},
 		property = {
 			Debug.COMMAND_SCOPE + "=sassc",
-			Debug.COMMAND_FUNCTION + "=compile"
+			Debug.COMMAND_FUNCTION + "=compile",
+			"main.thread=true"
 		}
 )
-public class SasscApplication {
+public class SasscApplication implements Runnable {
 
 	private SassCompiler sassc;
+	private String[] args;
 
 	public String compile(String input, String includePath, String imgPath)
 			throws SassCompilationException {
@@ -28,6 +35,22 @@ public class SasscApplication {
 	@Reference
 	void setSassCompiler(SassCompiler sassc) {
 		this.sassc = sassc;
+	}
+
+	@Override
+	public void run() {
+		if(args != null && args.length > 0) {
+			try {
+				System.out.println(sassc.compile(args[0], "", ""));
+			} catch (SassCompilationException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Reference
+	void setDone(Object done, Map<String, Object> parameters) {
+		args = (String[]) parameters.get("launcher.arguments");
 	}
 
 }
