86eb3aec2c3807ad10de5739c542f0699328029d
Gregory Amerson <gregory.amerson@liferay.com>
2014-12-31 12:01:04 +0800

    Added remaining tests

--- com.liferay.tools.sassc.provider/test/com/liferay/tools/sassc/provider/SassCompilerImplTest.java
+++ com.liferay.tools.sassc.provider/test/com/liferay/tools/sassc/provider/SassCompilerImplTest.java
@@ -21,4 +21,33 @@ public class SassCompilerImplTest extends SassLibraryTests {
 			".foo { background: white; } .foo .bar { margin-top: 10px; }",
 			stripNewLines(output));
 	}
+
+	@Test
+	public void testCompile_app_view_entry() throws Exception {
+		SassCompiler compiler = new SassCompilerImpl();
+		assertNotNull(compiler);
+
+		String output = compiler.compileFile(
+			BASE_RESOURCES + "/compass/_app_view_entry.scss",
+			BASE_RESOURCES + "/compass/common", "");
+		assertNotNull(output);
+		assertEquals(
+			stripNewLines(readFileContents(BASE_RESOURCES + "/compass/_app_view_entry.css")),
+			stripNewLines(output));
+	}
+
+	@Test
+	public void testCompileFile() throws Exception {
+		SassCompiler compiler = new SassCompilerImpl();
+		assertNotNull(compiler);
+
+		String output = compiler.compileFile(
+			BASE_RESOURCES + "_asset_category_selector.scss", "", "");
+		assertNotNull(output);
+		assertEquals(
+			stripNewLines(readFileContents(BASE_RESOURCES + "_asset_category_selector.css")),
+			stripNewLines(output));
+	}
+
+	private static final String BASE_RESOURCES = "bin_test/com/liferay/tools/sassc/provider/resources/";
 }
